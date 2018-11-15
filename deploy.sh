68227cbe9d438b1fa1d9ac64c683529b8812f439
Carlos Fernández Llamas <hello@sirikon.me>
2018-11-15 11:37:31 +0100

    Migrate to use Pitch.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
-gulp sass
+
+set -e
+
+npm run build
+cd dist/
 zip -r artifact css/ img/ index.html
 curl -F artifact=@./artifact.zip -F project=$MOLLY_PROJECT -F token=$MOLLY_TOKEN $MOLLY_URL"/deploy"
