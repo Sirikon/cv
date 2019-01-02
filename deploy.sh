7d0f372389e3d62f6c9088809009bbbd75312b5b
Carlos Fernández Llamas <hello@sirikon.me>
2019-01-02 01:18:04 +0100

    Update pitch, remove circleci

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-npm run build
-cd dist/
-zip -r artifact css/ img/ index.html
-curl -F artifact=@./artifact.zip -F project=$MOLLY_PROJECT -F token=$MOLLY_TOKEN $MOLLY_URL"/deploy"
